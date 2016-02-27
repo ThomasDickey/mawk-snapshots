a7750d5893c7fa5e739a3e7ff0e33f921ca27ea1
Thomas E. Dickey <dickey@invisible-island.net>
2016-02-27 01:53:19 -0500

    snapshot of project "mawk", label t20160226

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
 patchlev.h
-copyright 2009-2014,2015, Thomas E. Dickey
+copyright 2009-2015,2016, Thomas E. Dickey
 copyright 1991-1996,2014, Michael D. Brennan
 
 This is a source file for mawk, an implementation of
@@ -11,9 +11,9 @@ the GNU General Public License, version 2, 1991.
  */
 
 /*
- * $MawkId: patchlev.h,v 1.74 2015/05/03 22:04:28 tom Exp $
+ * $MawkId: patchlev.h,v 1.76 2016/02/27 01:51:50 tom Exp $
  */
 #define  PATCH_BASE	1
 #define  PATCH_LEVEL	3
 #define  PATCH_STRING	".4"
-#define  DATE_STRING    "20150503"
+#define  DATE_STRING    "20160226"
